3a0153770fbc318fe46378dd22f83c7892c4df4a
nprags <115040847+nprags@users.noreply.github.com>
2023-01-24 01:10:41 +0530

    Update Ironore-analysis-Forecasting-SImpleModel-forGrafana.py

--- Ironore-analysis-Forecasting-SImpleModel-forGrafana.py
+++ Ironore-analysis-Forecasting-SImpleModel-forGrafana.py
@@ -7,12 +7,8 @@
 import pandas as pd
 from prophet import Prophet
 import streamlit as st
-st.set_page_config(page_title="Forecasting Tool", layout="wide",
-                   initial_sidebar_state="auto",
-                   menu_items={
-        'Home': 'https://anmmic.grafana.net/',
-        'About': "Developed by A&M, India"
-    })
+st.set_page_config(page_title="Forecasting Tool", layout="centered",
+                   initial_sidebar_state="auto")
 
 # In[631]:
 st.header("Iron Ore Price Forecasting Tool")
