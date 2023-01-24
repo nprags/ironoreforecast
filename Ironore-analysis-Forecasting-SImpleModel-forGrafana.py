ccb133b39f181ee9a4fee46f7aa7736fbf90974d
nprags <115040847+nprags@users.noreply.github.com>
2023-01-24 09:58:34 +0530

    Update Ironore-analysis-Forecasting-SImpleModel-forGrafana.py

--- Ironore-analysis-Forecasting-SImpleModel-forGrafana.py
+++ Ironore-analysis-Forecasting-SImpleModel-forGrafana.py
@@ -248,7 +248,7 @@ if uploaded_file is not None:
        # In[665]:
        dfg2=pd.melt(dfg, id_vars =['Date'], value_vars =['yhat','yhat_upper','yhat_lower',predictC])
        #st.write(dfg2)
-       dfg2bar= dfg2.query('variable == @predictC || variable == "yhat"')
+       dfg2bar= dfg2.query('variable == @predictC | variable == "yhat"')
       
        #fname
 
